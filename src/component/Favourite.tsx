bf982f03ac3d17dd4841418b1cd200108296fa23
mesfin-2 <mesfintdev@gmail.com>
2025-03-16 08:42:38 +0200

    Searching feature in Favourie page implemented

--- src/component/Favourite.tsx
+++ src/component/Favourite.tsx
@@ -1,10 +1,13 @@
 import { Pause, Play, Search, Trash2 } from 'lucide-react';
+import { useState } from 'react';
 import { useMoodify } from '../hooks/useMoodify';
 import { SongItem } from '../types/moodify';
 import { formatTime } from '../utils/formatTime';
 import { Button } from './Button';
 
 const Favourite = () => {
+  const [searchTerm, setSearchTerm] = useState<string | ''>('');
+
   const {
     favourites,
     removeFromFavorites,
@@ -14,6 +17,16 @@ const Favourite = () => {
     currentSong,
     currentTime,
   } = useMoodify();
+
+  const filteredFavourite = searchTerm
+    ? favourites.filter((fav) =>
+        fav.artist.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : favourites;
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
 
   const handleTogglePlay = (song: SongItem) => {
     if (currentSong?.id === song.id) {
@@ -30,28 +43,32 @@ const Favourite = () => {
   return (
     <>
       {/* Search Form */}
-      <form className="relative mx-4 w-96">
-        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-secondary-text-light" />
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-full pl-10 pr-4 py-2 text-sm border border-secondary-border rounded-full text-secondary-text-light bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 font-sans"
-        />
-      </form>
+      <div className="flex flex-col xs:flex-1 md:justify-end md:items-end mx-4 mb-4">
+        <form className=" relative mx-4 w-56">
+          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-secondary-text-light" />
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleSearch}
+            className="w-full pl-10 pr-4 py-2 text-sm border border-secondary-border rounded-full text-secondary-text-light bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 font-sans"
+          />
+        </form>
+      </div>
       <table className="table-auto w-full text-left gap-2 ">
-        <thead className="mb-6">
-          <tr className="border-b mb-4">
-            <th>Song</th>
-            <th>Artist</th>
-            <th>Title</th>
-            <th>Play/Pause</th>
-            <th>Operation</th>
-            <th>Time</th>
+        <thead>
+          <tr className="border-b border-neutral-500">
+            <th className="px-4 py-2 hidden md:table-cell">Song</th>
+            <th className="px-4 py-2">Artist</th>
+            <th className="px-4 py-2">Title</th>
+            <th className="px-4 py-2">Play/Pause</th>
+            <th className="px-4 py-2">Operation</th>
+            <th className="px-4 py-2">Time</th>
           </tr>
         </thead>
         <tbody>
-          {favourites.length > 0 ? (
-            favourites.map((fav) => (
+          {filteredFavourite.length > 0 ? (
+            filteredFavourite.map((fav) => (
               <tr
                 key={fav.id}
                 className={
@@ -60,13 +77,13 @@ const Favourite = () => {
                     : 'bg-none'
                 }
               >
-                <td>
+                <td className="hidden md:table-cell">
                   <img
-                    className="w-8 h-8 rounded-md mr-2"
+                    className="w-8 h-8     rounded-md mr-2"
                     src={fav.thumbnailUrl}
                   />
                 </td>
-                <td>{fav.artist}</td>
+                <td className="text-wrap">{fav.artist}</td>
                 <td>{fav.title}</td>
                 <td>
                   <Button
